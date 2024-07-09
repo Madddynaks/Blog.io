80f53a62940f8ab973ae2e32fd2f101319948b2d
Madddynaks <madhav.nakra2002@gmail.com>
2024-07-09 22:54:21 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+
+const path = require("path");
+const express = require("express");
+const PORT = process.env.PORT || 8000;
+const userRoutes = require("./routes/user");
+const blogRoutes = require("./routes/blog");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+const Blog = require("./models/blog");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((e) => console.log("MongoDB Connected"));
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use("/user", userRoutes);
+app.use("/blog", blogRoutes);
+
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
